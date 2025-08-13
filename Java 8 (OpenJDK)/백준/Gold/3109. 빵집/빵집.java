77bdc0f6903db815cf3386c01df1a36143dd1299
Min Je Seong <minj21@naver.com>
2025-08-13 16:01:35 +0900

    [Gold II] Title: 빵집, Time: 400 ms, Memory: 49856 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Gold/3109. 빵집/빵집.java
+++ Java 8 (OpenJDK)/백준/Gold/3109. 빵집/빵집.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
     static int[] dx = {-1, 0, 1}; //오른쪽 위, 오른쪽, 오른쪽 아래,
     static int[] dy = {1, 1, 1};
-    static String[][] matrix;
+    static char[][] matrix;
     static boolean[][] visited;
     static int R, C;
     static int cnt = 0;
@@ -17,13 +17,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        matrix = new String[R][C];
+        matrix = new char[R][C];
         visited = new boolean[R][C];
         for (int i = 0; i < R; i++) {
-            String[] line = br.readLine().split("");
+            char[] line = br.readLine().toCharArray();
             for (int j = 0; j < C; j++) {
-                String token = line[j];
-                matrix[i][j] = token;
+                matrix[i][j] = line[j];
             }
         }
         for (int i = 0; i < R; i++) {
@@ -34,18 +33,17 @@ public class Main {
 
     public static boolean dfs(int x, int y) {
         visited[x][y] = true;
+        if (y == C - 1) {
+            cnt++;
+            return true;
+        }
+
         for (int i = 0; i < 3; i++) {
             int drx = dx[i] + x;
             int dry = dy[i] + y;
             if (valid(drx, dry) && !visited[drx][dry]
-                    && matrix[drx][dry].equals(".")) {
-                if (dry == C - 1) {
-                    cnt++;
-                    return true;
-                }
-                if (dfs(drx, dry)) {
-                    return true;
-                }
+                    && matrix[drx][dry] == '.') {
+                if(dfs(drx,dry)) return true;
             }
         }
         return false;
