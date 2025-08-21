fd5e0186cbc197f9e6479c6030bd73ec3805ad8b
Min Je Seong <minj21@naver.com>
2025-08-21 16:44:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14204 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ Java 8 (OpenJDK)/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,57 +2,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
-    static final int[] dx = {-1, 0, 1, 0};
-    static final int[] dy = {0, 1, 0, -1};
+    static boolean[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
     static int N;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int cnt = 0;
-    static int groupCnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        graph = new int[N][];
-        visited = new boolean[N][N];
-        List<Integer> result = new ArrayList<>(N);
+        map = new boolean[N][N];
+        List<Integer> result = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            graph[i] = Stream.of(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) == '1';
+            }
         }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && graph[i][j] == 1) {
-                    dfs(i, j);
-                    result.add(groupCnt+1);
-                    groupCnt = 0;
-                    cnt++;
+                if (map[i][j]) {
+                    result.add(dfs(i, j));
                 }
             }
         }
-        System.out.println(cnt);
-        result.stream().sorted().forEach(System.out::println);
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (Integer a : result) {
+            System.out.println(a);
+        }
 
 
     }
 
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;
+    public static int dfs(int x, int y) {
+        if(!map[x][y]){
+            return 0;
+        }
+        map[x][y] = false;
+        int sum = 0;
         for (int i = 0; i < 4; i++) {
-            int drx = dx[i] + x;
-            int dry = dy[i] + y;
-            if (vaild(drx, dry) && !visited[drx][dry] && graph[drx][dry] == 1) {
-                groupCnt++;
-                dfs(drx, dry);
+            int drx = x + dx[i];
+            int dry = y + dy[i];
+            if (isValid(drx, dry) && map[drx][dry]) {
+                sum += dfs(drx, dry);
             }
         }
+        return sum + 1;
     }
 
-    public static boolean vaild(int x, int y) {
+    public static boolean isValid(int x, int y) {
         return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
