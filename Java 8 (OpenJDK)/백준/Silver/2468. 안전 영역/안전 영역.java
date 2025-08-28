1cffb490c02287a73314b76f808cfc434fb8adaa
Min Je Seong <minj21@naver.com>
2025-08-28 14:29:39 +0900

    [Silver I] Title: 안전 영역, Time: 224 ms, Memory: 18656 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Silver/2468. 안전 영역/안전 영역.java
+++ Java 8 (OpenJDK)/백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,56 +2,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] graph;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[][] map;
     static boolean[][] visited;
-    static int n;
-    static int result;
+    static int N;
+    static int max = 0;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int result = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        graph = new int[n][];
-        visited = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, map[i][j]);
+            }
         }
-
-        for (int i = 0; i <= 100 ;i ++){
+        for (int i = 0; i <= max; i++) {
+            visited = new boolean[N][N];
             int cnt = 0;
-            for (int j = 0; j < n; j++){
-                for(int k = 0; k < n; k++){
-                    if(graph[j][k] > i && !visited[j][k] ){
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (!visited[j][k] && map[j][k] > i) {
                         dfs(j, k, i);
                         cnt++;
                     }
                 }
             }
-            result = Math.max(result, cnt);
-            visited = new boolean[n][n];
+            result = Math.max(cnt, result);
         }
-
         System.out.println(result);
-
     }
 
     public static void dfs(int x, int y, int boundary) {
         visited[x][y] = true;
-        for(int i = 0 ; i < 4 ; i++){
-            int drx = dx[i] + x;
-            int dry = dy[i] + y;
-            if(valid(drx,dry) && !visited[drx][dry] && graph[drx][dry] > boundary){
-                visited[drx][dry] = true;
-                dfs(drx, dry, boundary);
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (isValid(nx, ny) && !visited[nx][ny] && map[nx][ny] > boundary) {
+                dfs(nx, ny, boundary);
             }
         }
     }
 
-    public static boolean valid(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < n;
+    public static boolean isValid(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
     }
-
 }
