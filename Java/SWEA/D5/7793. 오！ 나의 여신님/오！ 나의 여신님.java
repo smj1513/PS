2b1be377190932b71abfc9f0c6218a260dd6d26a
Min Je Seong <minj21@naver.com>
2025-08-29 16:53:59 +0900

    [D5] Title: 오! 나의 여신님, Time: 119 ms, Memory: 26,880 KB -BaekjoonHub

--- Java/SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
+++ Java/SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Solution {
     static char[][] map;
-    static boolean[][] visitedWater;
+    static boolean[][] visitedDemon;
     static int[][] visited;
     static int R, C;
     static int[] dx = {-1, 0, 1, 0};
@@ -20,68 +20,68 @@ public class Solution {
             R = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
             map = new char[R][C];
-            visitedWater = new boolean[R][C];
+            visitedDemon = new boolean[R][C];
             visited = new int[R][C];
-            // 물 *, 돌X, 굴D, 고슴도치S, 빈곳 .
-            List<int[]> waters = new ArrayList<>();
-            int[] biber = null;
+            // 악마*, 돌X, 여신D, 수연S, 빈곳.
+            List<int[]> demon = new ArrayList<>();
+            int[] suyan = null;
             for (int i = 0; i < R; i++) {
                 map[i] = br.readLine().toCharArray();
                 for (int j = 0; j < C; j++) {
                     if (map[i][j] == '*') {
-                        waters.add(new int[]{i, j});
+                        demon.add(new int[]{i, j});
                     } else if (map[i][j] == 'S') {
-                        biber = new int[]{i, j};
+                        suyan = new int[]{i, j};
                     }
                 }
             }
-            int max = bfs(waters, biber);
+            int max = bfs(demon, suyan);
             System.out.println("#" + tc + " " + (max == -1 ? "GAME OVER" : max));
         }
     }
 
-    public static int bfs(List<int[]> water, int[] biber) {
-        Queue<int[]> waterQueue = new ArrayDeque<>(water);
-        for (int[] point : water) {
-            visitedWater[point[0]][point[1]] = true;
+    public static int bfs(List<int[]> demon, int[] suyan) {
+        Queue<int[]> demonQueue = new ArrayDeque<>(demon);
+        for (int[] point : demon) {
+            visitedDemon[point[0]][point[1]] = true;
         }
-        Queue<int[]> biberQueue = new ArrayDeque<>();
-        biberQueue.add(biber);
-        visited[biber[0]][biber[1]] = 0;
-        while (!waterQueue.isEmpty() || !biberQueue.isEmpty()) {
-            List<int[]> currentWaters = new ArrayList<>();
-            List<int[]> currentBibers = new ArrayList<>();
-            while (!waterQueue.isEmpty()) {
-                currentWaters.add(waterQueue.poll());
+        Queue<int[]> suyanQueue = new ArrayDeque<>();
+        suyanQueue.add(suyan);
+        visited[suyan[0]][suyan[1]] = 0;
+        while (!demonQueue.isEmpty() || !suyanQueue.isEmpty()) {
+            List<int[]> currentDemon = new ArrayList<>();
+            List<int[]> currentSuyan = new ArrayList<>();
+            while (!demonQueue.isEmpty()) {
+                currentDemon.add(demonQueue.poll());
             }
-            while (!biberQueue.isEmpty()) {
-                currentBibers.add(biberQueue.poll());
+            while (!suyanQueue.isEmpty()) {
+                currentSuyan.add(suyanQueue.poll());
             }
             //물이 먼저 퍼짐
-            for (int[] currentWater : currentWaters) {
+            for (int[] currentDe : currentDemon) {
                 for (int i = 0; i < 4; i++) {
-                    int nx = dx[i] + currentWater[0];
-                    int ny = dy[i] + currentWater[1];
-                    if (isValid(nx, ny) && !visitedWater[nx][ny] &&
+                    int nx = dx[i] + currentDe[0];
+                    int ny = dy[i] + currentDe[1];
+                    if (isValid(nx, ny) && !visitedDemon[nx][ny] &&
                             map[nx][ny] != 'X' &&
                             map[nx][ny] != 'D') {
-                        waterQueue.add(new int[]{nx, ny});
-                        visitedWater[nx][ny] = true;
+                        demonQueue.add(new int[]{nx, ny});
+                        visitedDemon[nx][ny] = true;
                         map[nx][ny] = '*';
                     }
                 }
             }
             //비버 이동
-            for (int[] currentBiber : currentBibers) {
+            for (int[] currentsu : currentSuyan) {
                 for (int i = 0; i < 4; i++) {
-                    int nx = dx[i] + currentBiber[0];
-                    int ny = dy[i] + currentBiber[1];
+                    int nx = dx[i] + currentsu[0];
+                    int ny = dy[i] + currentsu[1];
                     if (isValid(nx, ny) && visited[nx][ny] == 0 && map[nx][ny] != 'X' && map[nx][ny] != '*') {
                         if (map[nx][ny] == 'D') {
-                            return visited[currentBiber[0]][currentBiber[1]] + 1;
+                            return visited[currentsu[0]][currentsu[1]] + 1;
                         }
-                        visited[nx][ny] = visited[currentBiber[0]][currentBiber[1]] + 1;
-                        biberQueue.add(new int[]{nx, ny});
+                        visited[nx][ny] = visited[currentsu[0]][currentsu[1]] + 1;
+                        suyanQueue.add(new int[]{nx, ny});
                     }
                 }
             }
