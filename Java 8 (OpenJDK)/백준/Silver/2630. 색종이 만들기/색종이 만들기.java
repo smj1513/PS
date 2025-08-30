a5e9321336b0c7470b3b9c858813c9fbb9ab5c74
Min Je Seong <minj21@naver.com>
2025-08-30 21:46:09 +0900

    [Silver II] Title: 색종이 만들기, Time: 148 ms, Memory: 16680 KB -BaekjoonHub

--- Java 8 (OpenJDK)/백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ Java 8 (OpenJDK)/백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,51 +1,48 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 public class Main {
+	static int[][] map;
+	static int white, blue;
+	static int N;
 
-    static int[][] spaces;
-    static int green, white;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			map[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+		divide(0, 0, N);
+		System.out.println(white);
+		System.out.println(blue);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        spaces = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                spaces[i][j] = sc.nextInt();
-            }
-        }
-        green = white = 0;
-        makeSpace(0, 0, N);
-        System.out.println(white);
-        System.out.println(green);
-    }
+	}
 
-    //좌상단 좌표, 사이즈
-    static void makeSpace(int r, int c, int size) {
-        //step 1. 주어진 영역이 모두 같은 색상으로 이루어졌는지 체크
-        int sum = 0;
-        for (int i = r, rEnd = r + size; i < rEnd; i++) {
-            for (int j = c, cEnd = c + size; j < cEnd; j++) {
-                sum += spaces[i][j];
-            }
-        }
+	public static void divide(int x, int y, int size) {
+		int sum = 0;
+		for (int i = x, iEnd = x + size; i < iEnd; i++) {
+			for (int j = y, jEnd = y + size; j < jEnd; j++) {
+				sum += map[i][j];
+			}
+		}
 
-        //기저 : 모두 같은 색상이거나 크기가 1일 때
-        //step 2. 같은 색으로 이루어져있다면 해당 색에 카운트 증가 후 리턴
-        if (sum == size * size) { // 모두 초록색이면
-            ++green;
-            //step 3. 같은 색으로 이루어져있지 않다면, 4분할 후 각 공간에 대해 동일한 처리
-        } else if (sum == 0) { //모두 하얀색이면
-            ++white;
-        } else {//섞여있으면
-            int newSize = size / 2;
-            makeSpace(r, c, newSize);//영역 1
-            makeSpace(r + newSize, c, newSize);
-            makeSpace(r, c + newSize, newSize);
-            makeSpace(r + newSize, c + newSize, newSize);
-        }
+		if (sum == 0) {
+			white++;
+			return;
+		} else if (sum == size * size) {
+			blue++;
+			return;
+		} else {
+			int newSize = size / 2;
+			divide(x, y, newSize);
+			divide(x, y+newSize, newSize);
+			divide(x+newSize, y, newSize);
+			divide(x+newSize, y+newSize, newSize);
+		}
 
-
-    }
+	}
 }
