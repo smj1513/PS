da412acd72c2f1d11739c7ecdd16a52f00f890cf
Min Je Seong <minj21@naver.com>
2025-09-01 17:12:09 +0900

    [Gold IV] Title: 키 순서, Time: 712 ms, Memory: 42540 KB -BaekjoonHub

--- Java/백준/Gold/2458. 키 순서/키 순서.java
+++ Java/백준/Gold/2458. 키 순서/키 순서.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -10,18 +10,19 @@ public class Main {
 	static ArrayList<Integer>[] heights, reverse;
 	static boolean[] visited;
 	static int result;
-	
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
 		heights = new ArrayList[n + 1];
 		reverse = new ArrayList[n + 1];
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			heights[i] = new ArrayList<>();
 			reverse[i] = new ArrayList<>();
 		}
@@ -36,9 +37,11 @@ public class Main {
 		}
 		
 		result = 0;
-		for(int i = 1; i < n + 1; i++) {
-			int bigger = bfs(i, heights);
-			int smaller = bfs(i, reverse);
+		for(int i = 1; i <= n; i++) {
+			visited = new boolean[n + 1];
+			
+			int bigger = dfs(i, reverse);
+			int smaller = dfs(i, heights);
 			
 			if(bigger + smaller == n - 1) {
 				result++;
@@ -49,26 +52,17 @@ public class Main {
 		
 	}
 	
-	public static int bfs(int start, ArrayList<Integer>[] list) {
-		ArrayDeque<Integer> q = new ArrayDeque<>();
-		visited = new boolean[n + 1];
-		
-		q.offer(start);
-		visited[start] = true;
-		
+	public static int dfs(int start, ArrayList<Integer>[] list) {
 		int cnt = 0;
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for(int next : list[cur]) {
-				if(visited[next]) {
-					continue;
-				}
-				
-				visited[next] = true;
-				q.offer(next);
-				cnt++;
+		
+		for(int next : list[start]) {
+			if(visited[next]) {
+				continue;
 			}
+			
+			visited[next] = true;
+			cnt++;
+			cnt += dfs(next, list);
 		}
 		
 		return cnt;
