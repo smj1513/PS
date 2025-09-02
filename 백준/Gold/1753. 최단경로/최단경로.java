9c9d25b39bdcacdf0827d959c10505333980f99e
Min Je Seong <minj21@naver.com>
2025-09-02 09:27:07 +0900

    [Gold IV] Title: 최단경로, Time: 820 ms, Memory: 116100 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,78 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+/*
+ * 최단 경로 문제
+ *
+ * */
+
+class Edge implements Comparable<Edge> {
+    int vertax;
+    int weight;
+
+    public Edge(int vertax, int weight) {
+        this.vertax = vertax;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return Integer.compare(this.weight, o.weight);
+    }
+}
+
+
+public class Main {
+    static List<LinkedList<Edge>> graph = new ArrayList<>();
+    static int[] distance;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+        for (int i = 0; i <= V; i++) {
+            graph.add(new LinkedList<>());
+        }
+        distance = new int[V + 1];
+        visited = new boolean[V + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE); //최단 경로 배열을 초기화
+        distance[start] = 0; //시작점의 최단 경로는 0
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph.get(u).addFirst(new Edge(v, w));
+        }
+        dijkstra(start);
+        for (int i = 1; i <= V; i++) {
+            System.out.println(distance[i] == Integer.MAX_VALUE ? "INF" : distance[i]);
+        }
+    }
+
+    //다익스트라는 기본적으로 그리디 알고리즘
+    //현재의 최단 경로가 이후의 최단경로를 보장한다 가정하여 구현
+    static void dijkstra(int start) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>(); //우선 순위 큐를 사용하여 구현
+        pq.offer(new Edge(start, 0));
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+            if (!visited[current.vertax]) {
+                visited[current.vertax] = true;
+                for (Edge next : graph.get(current.vertax)) {
+                    if (distance[next.vertax] > distance[current.vertax] + next.weight) {
+                        distance[next.vertax] = distance[current.vertax] + next.weight;
+                        pq.add(new Edge(next.vertax, distance[next.vertax]));
+                    }
+                }
+            }
+        }
+    }
+}
