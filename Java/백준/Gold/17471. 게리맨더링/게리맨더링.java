343b43caa112f2c9f0f7c31098a90b1ab436c657
Min Je Seong <minj21@naver.com>
2025-09-02 14:55:59 +0900

    [Gold III] Title: 게리맨더링, Time: 116 ms, Memory: 15724 KB -BaekjoonHub

--- Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,50 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static int n;
 	static int[] population;
-	static ArrayList<Integer>[] nearby;
-	static int result = Integer.MAX_VALUE;
+	static ArrayList<Integer>[] area;
 	static boolean[] selectedA;
-	
+	static int result;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		n = Integer.parseInt(br.readLine());
 		
 		st = new StringTokenizer(br.readLine());
 		population = new int[n + 1];
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			population[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		nearby = new ArrayList[n + 1];
-		for(int i = 1; i < n + 1; i++) {
-			nearby[i] = new ArrayList<>();
+		area = new ArrayList[n + 1];
+		for(int i = 1; i <= n; i++) {
+			area[i] = new ArrayList<>();
 		}
 		
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int cnt = Integer.parseInt(st.nextToken());
 			for(int j = 0; j < cnt; j++) {
-				nearby[i].add(Integer.parseInt(st.nextToken()));
+				area[i].add(Integer.parseInt(st.nextToken()));
 			}
 		}
 		
 		selectedA = new boolean[n + 1];
+		result = Integer.MAX_VALUE;
 		dfs(1);
-		
+				
 		System.out.println(result == Integer.MAX_VALUE ? -1 : result);
-
 	}
 	
 	public static void dfs(int idx) {
@@ -52,6 +51,7 @@ public class Main {
 			if(bfs(true) && bfs(false)) {
 				int popA = getPop(true);
 				int popB = getPop(false);
+				
 				result = Math.min(result, Math.abs(popA - popB));
 			}
 			
@@ -66,11 +66,11 @@ public class Main {
 	}
 	
 	public static boolean bfs(boolean isA) {
-		boolean[] visited = new boolean[n + 1];
 		ArrayDeque<Integer> q = new ArrayDeque<>();
+		boolean[] visited = new boolean[n + 1];
 		
 		int start = -1;
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			if(selectedA[i] == isA) {
 				start = i;
 				break;
@@ -81,13 +81,13 @@ public class Main {
 			return false;
 		}
 		
-		visited[start] = true;
 		q.offer(start);
+		visited[start] = true;
 		
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			
-			for(int next: nearby[cur]) {
+			for(int next : area[cur]) {
 				if(visited[next]) {
 					continue;
 				}
@@ -96,12 +96,12 @@ public class Main {
 					continue;
 				}
 				
-				visited[next] = true;
 				q.offer(next);
+				visited[next] = true;
 			}
 		}
 		
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			if(selectedA[i] == isA && !visited[i]) {
 				return false;
 			}
@@ -112,7 +112,7 @@ public class Main {
 	
 	public static int getPop(boolean isA) {
 		int sum = 0;
-		for(int i = 1; i < n + 1; i++) {
+		for(int i = 1; i <= n; i++) {
 			if(selectedA[i] == isA) {
 				sum += population[i];
 			}
