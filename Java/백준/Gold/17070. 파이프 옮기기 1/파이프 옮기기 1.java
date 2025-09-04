d1465875012d0280e77bc7820efd307093e78cd8
Min Je Seong <minj21@naver.com>
2025-09-04 16:39:54 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,55 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int n, cnt;
+	
+	static int n;
 	static int[][] map;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		n = Integer.parseInt(br.readLine());
 		
-		map = new int[n][n];
-		for(int i = 0; i < n; i ++) {
+		map = new int[n + 1][n + 1];
+		for(int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < n; j++) {
+			for(int j = 1; j <= n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		cnt = 0;
-		dfs(0, 1, 0);
+		int[][][] dp = new int[n + 1][n + 1][3];
+		dp[1][2][0] = 1;
 		
-		System.out.println(cnt);
-	}
-	
-	public static void dfs(int x, int y, int dir) {
-		if(x == n - 1 && y == n - 1) {
-			cnt++;
-			return;
-		}
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(map[i][j] == 1) {
+					continue;
+				}
 				
-		
-		if(dir == 0 || dir == 2) {
-			if(y < n - 1 && map[x][y + 1] == 0) {
-				dfs(x, y + 1, 0);
+				if(j - 1 >= 1 && map[i][j] == 0) {	// 가로 이동
+					dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
+				}
+				
+				if(i - 1 >= 1 && map[i][j] == 0) {	// 세로 이동
+					dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
+				}
+				
+				if(i - 1 >= 1 && j - 1 >= 1) {		// 대각선 이동
+					if(map[i][j] == 0 && map[i - 1][j] == 0 && map[i][j - 1] == 0) {
+						dp[i][j][2] += dp[i - 1][j - 1][2] + dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1];
+					}
+				}
 			}
 		}
 		
-		if(dir == 1 || dir == 2) {
-			if(x < n - 1 && map[x + 1][y] == 0) {
-				dfs(x + 1, y, 1);
-			}
-		}
+		int result = dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
 		
-		if(dir == 0 || dir == 1 || dir == 2){
-			if(x < n - 1 && y < n - 1 && map[x + 1][y] == 0 && map[x][y + 1] == 0 && map[x + 1][y + 1] == 0) {
-				dfs(x + 1, y + 1, 2);
-			}
-		}
+		System.out.println(result);
+
 	}
 
 }
