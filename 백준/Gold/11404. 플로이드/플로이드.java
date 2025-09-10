d8a3690f7d5386b9a1baaebc25fd43a2be2b9bc0
Min Je Seong <minj21@naver.com>
2025-09-10 14:31:59 +0900

    [Gold IV] Title: 플로이드, Time: 500 ms, Memory: 46584 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] distance;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        distance = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                } else {
+                    distance[i][j] = INF;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            distance[from][to] = Math.min(distance[from][to], cost);
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((distance[i][j] == INF ? 0 : distance[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
