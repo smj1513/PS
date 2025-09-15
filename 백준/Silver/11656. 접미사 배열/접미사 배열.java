1e338dc503361a0b9adec826a0eacb5cf1c5ef05
Min Je Seong <minj21@naver.com>
2025-09-15 16:36:10 +0900

    [Silver IV] Title: 접미사 배열, Time: 176 ms, Memory: 16044 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] suffixArray = new String[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            suffixArray[i] = str.substring(i);
+        }
+        Arrays.sort(suffixArray);
+        for (String st : suffixArray) {
+            System.out.println(st);
+        }
+    }
+}
