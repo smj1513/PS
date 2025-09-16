b5dd339c37acd87af0d2e88727702a623ed1b1ce
Min Je Seong <minj21@naver.com>
2025-09-16 14:46:50 +0900

    [D2] Title: 파리 퇴치, Time: 87 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ Java/SWEA/D2/2001. 파리 퇴치/README.md
@@ -0,0 +1,15 @@
+# [D2] 파리 퇴치 - 2001 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq) 
+
+### 성능 요약
+
+메모리: 26,368 KB, 시간: 87 ms, 코드길이: 1,210 Bytes
+
+### 제출 일자
+
+2025-09-16 14:46
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ Java/SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int arr[][] = new int[N][N];
+			
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int max = 0;
+			for(int i = 0; i <= N-M; i++) {
+				for(int j = 0; j <= N-M; j++) {
+					int sum = 0;
+                    for (int r = i; r < i + M; r++) {
+                        for (int c = j; c < j + M; c++) {
+                            sum += arr[r][c];
+                        }
+                    }
+                    max = Math.max(max, sum);
+				}
+			}
+			
+			System.out.println("#" + test_case +" " + max);
+		}
+	}
+
+}
