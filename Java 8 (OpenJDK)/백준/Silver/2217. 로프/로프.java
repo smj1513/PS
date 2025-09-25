0db689a440a5ef3a49fd8609f20d7d71243ffacc
Min Je Seong <minj21@naver.com>
2025-09-25 23:50:17 +0900

    [Silver IV] Title: 로프, Time: 376 ms, Memory: 27676 KB -BaekjoonHub

---
+++ Java 8 (OpenJDK)/백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Integer[] roop = new Integer[N];
+
+		for (int i = 0; i < N; i++) {
+			roop[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(roop, Collections.reverseOrder());
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, roop[i] * (i + 1));
+		}
+		System.out.println(max);
+	}
+}
