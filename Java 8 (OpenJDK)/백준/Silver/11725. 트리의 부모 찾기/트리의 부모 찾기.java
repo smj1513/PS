f1f0c39b5c87d7c7ed3ab6ae261c7cc5fb932b5c
Min Je Seong <minj21@naver.com>
2025-10-11 00:27:42 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1296 ms, Memory: 74684 KB -BaekjoonHub

---
+++ Java 8 (OpenJDK)/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static List<Integer>[] graph;
+	static int[] parent;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		graph = new List[N+1];
+		visited = new boolean[N+1];
+		for (int i = 0 ; i <= N ;i++){
+			graph[i] = new LinkedList<>();
+		}
+		parent = new int[N+1];
+		for (int i = 0 ; i < N-1; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			graph[to].add(from);
+			graph[from].add(to);
+		}
+		dfs(1);
+		for (int i = 2;  i <=N;i++){
+			int a = parent[i];
+			System.out.println(a);
+		}
+	}
+	public static void dfs(int v){
+		visited[v] = true;
+		for (Integer nv : graph[v]){
+			if(!visited[nv]){
+				dfs(nv);
+				parent[nv] = v;
+			}
+		}
+	}
+}
