886ad29984b69d4fdd6365bbc1b13560f125c73c
Min Je Seong <minj21@naver.com>
2025-10-26 16:12:21 +0900

    [Silver II] Title: 주식, Time: 2996 ms, Memory: 153708 KB -BaekjoonHub

---
+++ Python/백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,16 @@
+T = int(input())
+for t in range(T):
+    N = int(input())
+    prices = list(map(int, input().split()))
+
+    max_profit = 0
+    max_price = prices[N - 1] 
+
+    # 마지막 날의 전날(N-2)부터 첫날(0)까지 거꾸로 순회
+    for i in range(N - 2, -1, -1):
+        if prices[i] < max_price:
+            max_profit += (max_price - prices[i])
+        else:
+            max_price = prices[i]
+
+    print(max_profit)
